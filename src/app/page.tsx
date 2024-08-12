ea53017614e00674fa5e4f5d515b212eb3e09d36
rn-santos227 <rn_santos227@yahoo.com>
2024-08-12 20:02:21 +0800

    updated page in app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,8 @@ export default function Index() {
   const router = useRouter();
   return (
     <>
-
+      <main className="background flex min-h-screen flex-col items-center justify-between p-24">
+      </main>
     </>
   );
 }
