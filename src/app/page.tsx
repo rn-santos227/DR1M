dadbb393159a51d2cea3e2b9fe3302bae2f8e5e7
rn-santos227 <rn_santos227@yahoo.com>
2024-08-12 20:04:23 +0800

    updated page in app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,15 @@
 
 import { useRouter } from 'next/navigation';
 
+import Image from 'next/image';
+
 export default function Index() {
   const router = useRouter();
   return (
     <>
+      <header className="fixed top-0 left-0 w-full text-white p-2 flex justify-between items-center bg-black shadow-light-green z-50">
+        
+      </header>
       <main className="background flex min-h-screen flex-col items-center justify-between p-24">
       </main>
     </>
