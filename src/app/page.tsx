30372520de709d12fd94b00e72b384546abb1307
rn-santos227 <rn_santos227@yahoo.com>
2024-08-12 20:12:49 +0800

    updated page in app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,9 @@ export default function Index() {
   return (
     <>
       <header className="fixed top-0 left-0 w-full text-white p-2 flex justify-between items-center bg-black shadow-light-green z-50">
-        
+        <div className="flex items-center">
+          <Image src="/assets/images/logo.png" alt="Logo" width={75} height={75} />
+        </div>
       </header>
       <main className="background flex min-h-screen flex-col items-center justify-between p-24">
       </main>
