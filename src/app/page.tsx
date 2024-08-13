efa4e40789ca1867134533b316e7f36ac5773a4e
rn-santos227 <rn_santos227@yahoo.com>
2024-08-13 23:01:52 +0800

    updated logo-ligh in image asset

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { useRouter } from 'next/navigation';
 
 import Image from 'next/image';
 
-import Button from '@/components/ui/Button';
+import Button from '@/components/Button';
 
 export default function Index() {
   const router = useRouter();
@@ -21,7 +21,7 @@ export default function Index() {
     <>
       <header className="fixed top-0 left-0 w-full text-white p-2 flex justify-between items-center bg-black shadow-light-green z-50">
         <div className="flex items-center">
-          <Image src="/assets/images/logo.png" alt="Logo" width={50} height={50} />
+          <Image src="/assets/images/logo-light.png" alt="Logo" width={50} height={50} />
           <Image src="/assets/images/site-title.png" alt="Title" width={250} height={250} />
         </div>
         <div className="flex space-x-4 mx-4">
