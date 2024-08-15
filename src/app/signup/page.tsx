9a36bfdb7a810fd4a57ddda22cbc14e829f9b544
rn-santos227 <rn_santos227@yahoo.com>
2024-08-15 22:22:45 +0800

    updated signup page

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -11,7 +11,7 @@ import TextField from '@/components/TextField';
 const SignUp: React.FC = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-
+  const [confirmPassword, setConfirmPassword] = useState('');
 
   return (
     <>
